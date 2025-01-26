0c3e402abcdb5a3a467f1c966e528939478e7b98
lDEVinux <likurgames@gmail.com>
2025-01-25 22:46:14 -0500

    Update typingshit.js

--- typingshit.js
+++ typingshit.js
@@ -32,3 +32,12 @@ function typeLoop() {
 }
 
 typeLoop();
+
+function hamburg(){
+                const navbar = document.querySelector(".dropdown")
+                navbar.style.transform = "translateY(0px)"
+            }
+            function cancel(){
+                const navbar = document.querySelector(".dropdown")
+                navbar.style.transform = "translateY(-500px)"
+            }
